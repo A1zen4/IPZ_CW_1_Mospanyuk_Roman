ceb2755e6a2916ae0f89acd0cfd06010871263bd
A1zen4 <145866073+A1zen4@users.noreply.github.com>
2024-04-09 16:31:19 +0300

    Base Field

--- app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
@@ -3,13 +3,29 @@ package ua.edu.lntu.ipz_cw_1_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import ua.edu.lntu.ipz_cw_1_2.ui.theme.IPZ_CW_1_2_Mospanyuk_RomanTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +48,17 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+    var email by remember { mutableStateOf(TextFieldValue()) }
+    var password by remember { mutableStateOf(TextFieldValue()) }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    Text(text = "Sing In")
+                    Text(
+                        text = "Sing In",
+                        fontSize = 24.sp
+                    )
                 })
         }
     ){
@@ -47,11 +69,24 @@ fun Greeting() {
             modifier = Modifier
                 .padding(paddingValues)
         ) {
-
+            TextField(
+                value = email,
+                onValueChange = {email = it},
+                label = {Text("Введіть email")}
+            )
+            TextField(
+                value = password,
+                onValueChange = {password = it},
+                label = {Text("Введіть password")}
+            )
+            Text(text = "Ви ввійшли")
+            Button(onClick = { /*TODO*/ }) {
+                Text("Sing In")
+                
+            }
         }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
